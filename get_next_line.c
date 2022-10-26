3ffc4c9335de5d67d3de4e9e4e0956879603a27b
bsirikam <buntakan0703@gmail.com>
2022-10-27 01:40:55 +0700

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,33 @@
 /*   By: bsirikam <bsirikam@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 17:49:25 by bsirikam          #+#    #+#             */
-/*   Updated: 2022/10/24 11:56:05 by bsirikam         ###   ########.fr       */
+/*   Updated: 2022/10/26 02:03:11 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_build(char *str)
-{
-	char	*new;
-
-	if (str != NULL)
-		return (str);
-	new = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!new)
-		return (NULL);
-	if (new)
-		*new = '\0';
-	return (new);
-}
-
-char	*let_read(char *str, int fd)
+char	*ha_nextnaja(char *str, int fd)
 {
 	char	*new_str;
+	char	*torpai;
+	char	*buntad;
 
 	new_str = NULL;
 	if (ft_strlen(str))
 	{
-		
+		torpai = ft_strchr(str, '\n');
+		if (!torpai)
+		{
+			new_str = ft_strjoin(str, "");
+			
+		}
+		if (torpai)
+		{
+			*torpai = '\0';
+			buntad = ft_strjoin(str, "\n");
+			return (buntad);
+		}
 	}
 }
 
@@ -43,9 +42,13 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	str = ft_build(str);
 	if (!str)
-		return (NULL);
-	str = let_read(str, fd);
+	{
+		str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!str)
+			return (NULL);
+		*str = '\0';
+	}
+	str = ha_nextnaja(str, fd);
 	return (NULL);
 }

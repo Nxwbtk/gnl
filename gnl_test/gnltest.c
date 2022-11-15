fd2e3f08a0f2bd72d597db8b51d8dfa6841be0b0
bsirikam <buntakan0703@gmail.com>
2022-11-16 03:30:02 +0700

    new version added

--- gnl_test/gnltest.c
+++ gnl_test/gnltest.c
@@ -1,42 +1,30 @@
 #include "get_next_line.h"
 
-char	*go_read(int fd, char *buff)
-{
-	int		n;
-	char	*next;
-	size_t	len_nl;
-	size_t	buff_len;
-
-	// printf("%d\n", fd);
-	(void)buff;
-	buff = malloc(BUFFER_SIZE+1);
-	n = read(fd, buff, BUFFER_SIZE);
-	printf("%s\n", buff);
-	buff_len = ft_strlen(buff);
-	if (buff_len)
-	{
-		len_nl = ft_strchr(buff, '\n');
-		if (len_nl != 0)
-		{
-			buff[len_nl] = '\0';
-			next = ft_strjoin(buff + len_nl + 1, "");
-			return (buff);
-		}
-		buff[buff_len] = '\0';
-	}
-	return (buff);
-}
-
 char	*build(char *buff)
 {
 	char	*new;
 
-	if (buff != NULL)
+	if (buff)
 		return (buff);
 	new = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (new != NULL)
-		new[0] = '\0';
 	return (new);
+}
+
+char	*go_read(int fd)
+{
+	char	*buff;
+	char	*next;
+	size_t	len_nl;
+
+	buff = build(buff);
+	read(fd, buff, BUFFER_SIZE);
+	len_nl = ft_strchr(buff, '\n');
+	if (len_nl > 0)
+	{
+		buff[len_nl] = '\0';
+		next = ft_strjoin("", buff + len_nl + 1);
+	}
+	return (buff);
 }
 
 char	*gnl(int fd)
@@ -44,20 +32,12 @@ char	*gnl(int fd)
 	static char	*line;
 	char		*buff;
 
-	// printf("%d\n", fd);
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
-	line = NULL;
-	// buff = build(buff);
-	// printf("%d\n", fd);
-	buff = go_read(fd, buff);
 	if (!line)
-	{
-		line = ft_strjoin(line, buff);
-		return (line);
-	}
-	else
-		line = ft_strjoin(line, buff);
+		line = build(line);
+	buff = go_read(fd);
+	line = ft_strjoin(line, buff);
 	return (line);
 }
 
@@ -69,6 +49,8 @@ int	main(void)
 	fd = open("test.txt", O_RDONLY);
 	// printf("%d\n", fd);
 	s = gnl(fd);
+	printf("%s\n", s);
+	// s = gnl(fd);
 	// printf("%s\n", s);
 	free(s);
 	close(fd);

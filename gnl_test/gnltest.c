d57411939cc4f492c424b6136375f9e3b56034b5
bsirikam <buntakan0703@gmail.com>
2022-11-18 20:05:40 +0700

    add somethin

--- gnl_test/gnltest.c
+++ gnl_test/gnltest.c
@@ -4,40 +4,60 @@ char	*build(char *buff)
 {
 	char	*new;
 
-	if (buff)
+	if (buff != NULL)
 		return (buff);
 	new = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (new != NULL)
+		new[0] = '\0';
 	return (new);
 }
 
 char	*go_read(int fd)
 {
+	char	*tmp;
 	char	*buff;
-	char	*next;
 	size_t	len_nl;
 
 	buff = build(buff);
-	read(fd, buff, BUFFER_SIZE);
-	len_nl = ft_strchr(buff, '\n');
-	if (len_nl > 0)
+	// printf("%s\n", buff);
+	tmp = build(tmp);
+	// printf("%s\n", tmp);
+	while (ft_strchr(tmp, '\n') == 0)
 	{
-		buff[len_nl] = '\0';
-		next = ft_strjoin("", buff + len_nl + 1);
+		read(fd, tmp, BUFFER_SIZE);
+		// printf("%s\n", tmp);
+		if (ft_strchr(tmp, '\n') != 0)
+		{
+			// printf("%s\n", buff);
+			// printf("%zu\n", ft_strchr(tmp, '\n'));
+			tmp[ft_strchr(tmp, '\n')] = '\0';
+			// printf("%s\n", tmp);
+			buff = ft_strjoin(buff, tmp);
+			// printf("%s\n", buff);
+			return (buff);
+		}
+		buff = ft_strjoin(buff, tmp);
+		// printf("Run again\n");
 	}
 	return (buff);
 }
 
 char	*gnl(int fd)
 {
-	static char	*line;
+	static char	*line = NULL;
 	char		*buff;
 
-	if (fd < 0 || BUFFER_SIZE < 0)
+	if (fd < 0 || BUFFER_SIZE < 0 || read(fd, NULL, 0) < 0)
 		return (NULL);
+	// printf("%s\n", line);
 	if (!line)
 		line = build(line);
+	// printf("%s\n", line);
 	buff = go_read(fd);
+	// printf("%s\n", buff);
+	printf("%s\n", line);
 	line = ft_strjoin(line, buff);
+	// printf("%s\n", line);
 	return (line);
 }
 
@@ -49,7 +69,7 @@ int	main(void)
 	fd = open("test.txt", O_RDONLY);
 	// printf("%d\n", fd);
 	s = gnl(fd);
-	printf("%s\n", s);
+	// printf("%s\n", s);
 	// s = gnl(fd);
 	// printf("%s\n", s);
 	free(s);

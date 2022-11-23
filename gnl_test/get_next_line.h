94623c170a62ffab40d3caa77e438d2ce8bbc56c
Bunthakan Sirikamonthip <bsirikam@z2t10c3.42bangkok.com>
2022-11-23 17:16:19 +0700

    fix some

--- gnl_test/get_next_line.h
+++ gnl_test/get_next_line.h
@@ -6,18 +6,21 @@
 /*   By: bsirikam <bsirikam@student.42bangkok.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 19:48:39 by bsirikam          #+#    #+#             */
-/*   Updated: 2022/11/23 16:50:59 by bsirikam         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:15:35 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFFER_SIZE 42
+//# ifndef BUFFER_SIZE
+//# define BUFFER_SIZE 42
+//# endif
 # include <stdlib.h>
 # include <stdio.h>
 # include <fcntl.h>
 # include <unistd.h>
 
+char	*get_next_line(int fd);
 size_t	ft_strlen(const char *str);
 size_t	ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
